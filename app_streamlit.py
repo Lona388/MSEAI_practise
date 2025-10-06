2da43d928080b951abfcd527d2058d871909c26a
Lona388 <138610794+Lona388@users.noreply.github.com>
2025-10-06 18:56:54 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("MY PROJECT")
-st.header("This is a header")
-st.subheader("This is a subheader")
-st.text("I love AI VIET NAM")
+def get_name():
+    st.write("Thai")
+
+agree = st.checkbox("I agree", on_change=get_name)
+
+if agree:
+    st.write("Great!")
+
+st.radio(
+    "Your favorite color:",
+    ['Yellow', 'Blue'],
+    captions=['Vàng', 'Xanh']
+)
