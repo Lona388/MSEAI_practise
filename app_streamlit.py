23a8db8cfeb3d8c8785c0a53a20c146176f9100d
Lona388 <138610794+Lona388@users.noreply.github.com>
2025-10-06 18:52:03 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,22 +1,6 @@
 import streamlit as st
-#from fact import fact
 
-def fact(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n*fact(n-1)
-
-def main():
-    st.title('Factorial Caculator')
-    number = st.number_input("Enter a number:",
-                             min_value = 0,
-                             max_value = 900)
-    
-    if st.button("Calculate"):
-        result = fact(number)
-        st.write(f"The factorial of {number} is {result}.")
-        st.balloons()
-
-if __name__ == "__main__":
-    main()
+st.title("MY PROJECT")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.text("I love AI VIET NAM")
